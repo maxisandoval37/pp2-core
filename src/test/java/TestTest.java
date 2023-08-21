ef6043e5d34e3f2b9d031738be9643061327a0ce
FrancoBre <bregolif.fb@gmail.com>
2023-08-21 12:35:42 -0300

    added simple test to test the tests during build

---
+++ src/test/java/TestTest.java
@@ -0,0 +1,13 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class TestTest {
+
+    @Test
+    void testAddition() {
+        int result = 2 + 3;
+        assertEquals(5, result);
+    }
+
+}
