6d440cbfd582e442b237c4e7b141d27893bb5d70
FrancoBre <bregolif.fb@gmail.com>
2023-09-12 20:36:26 -0300

    cosas cosas cosas

--- src/main/java/shoppinator/core/ShopScraper.java
+++ src/main/java/shoppinator/core/ShopScraper.java
@@ -4,24 +4,30 @@ import shoppinator.core.factory.ProductFactory;
 import shoppinator.core.interfaces.Scrapper;
 import shoppinator.core.interfaces.Shop;
 import java.util.List;
-import javax.swing.text.html.parser.Element;
 import shoppinator.core.model.Product;
 
 public class ShopScrapper extends Shop {
 
-    Scrapper scrapper;
-    ProductFactory productFactory;
+    private Scrapper scrapper;
+    private ProductFactory productFactory;
 
     public ShopScrapper(Scrapper scrapper) {
+        this.searchFeaturedProducts();
+
         this.scrapper = scrapper;
     }
 
     @Override
     public List<Product> search(String productName) {
-        Element element = scrapper.scrap(productName);
-        Product product = productFactory.create(element);
-        this.addProduct(product);
+        String scrapedProduct = scrapper.scrap(productName);
+        List<Product> products = productFactory.create(scrapedProduct);
 
+        this.addProducts(products);
         return this.getProducts();
+    }
+
+    private List<Product> searchFeaturedProducts() {
+        String featuredProducts = this.scrapper.scrapFeatured();
+        return productFactory.create(featuredProducts);
     }
 }
