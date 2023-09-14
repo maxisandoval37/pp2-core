d607c20e7a86e3867744da8625afaf5aaf92f4c6
FrancoBre <bregolif.fb@gmail.com>
2023-09-14 20:15:23 -0300

    Add snake case convention to object mapper
    And delete shop proxy class

--- src/main/java/shoppinator/core/factory/ProductFactory.java
+++ src/main/java/shoppinator/core/factory/ProductFactory.java
@@ -3,6 +3,7 @@ package shoppinator.core.factory;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import java.util.Collections;
 import java.util.List;
 import lombok.extern.slf4j.Slf4j;
@@ -19,6 +20,7 @@ public class ProductFactory {
 
     public List<Product> create(String productJson) {
         try {
+            objectMapper.setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE);
             return this.objectMapper.readValue(productJson, new TypeReference<List<Product>>(){});
         } catch (JsonProcessingException e) {
             log.error(e.getMessage());
