79920a608bc6e465624a5de50a867bf342512fdd
FrancoBre <bregolif.fb@gmail.com>
2023-09-14 09:36:48 -0300

    funca el discovery trayendo desde jar 😎
    - feliz día del programador are que fue ayer
    falta:
    - cambiar los scappers de nombre por scraper
    - ver qué hacemos con la lógica de scrap featured
    - hacer tests
    - armar bien el jar con las clases que devuelvan el mock de un json

--- src/main/java/service/discovery/ScrapperDiscoverer.java
+++ src/main/java/service/discovery/ScrapperDiscoverer.java
@@ -1,72 +1,91 @@
 package service.discovery;
 
-import java.net.MalformedURLException;
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
 import java.net.URL;
 import java.net.URLClassLoader;
-import java.util.Collection;
-import java.util.Collections;
-import shoppinator.core.interfaces.Scrapper;
-import java.io.File;
+import java.util.Enumeration;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+import lombok.NoArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
-import utils.FullyQualifiedNameFinder;
+import shoppinator.core.interfaces.Scrapper;
 
 @Slf4j
+@NoArgsConstructor
 public class ScrapperDiscoverer {
 
-    public ScrapperDiscoverer() {
-    }
-
+    @SneakyThrows
     public Set<Scrapper> discover(String path) {
-        //this.v2(path);
         return this.findClasses(path);
     }
 
-    @SneakyThrows
-    private Set<Scrapper> findClasses(String path) {
-        System.getProperty("java.class.path");
-        Set<Scrapper> result = new HashSet<>();
-        for (File f : new File(path).listFiles()) {
-            if(f.isDirectory()) {
-                return findClasses(f.getPath());
-            }
-            if (!f.getName().endsWith(".class")) continue;
-
-            Class<?> cls = null;
-            try
-            {
-                String fileName = f.getName().substring(0, f.getName().lastIndexOf("."));
-                URL[] urls = { new File(path).toURI().toURL() };
-                ClassLoader urlClassLoader = new URLClassLoader(urls);
-                cls = urlClassLoader.loadClass(fileName);
-            }
-            catch (ClassNotFoundException | MalformedURLException e)
-            {
-                log.error(e.getMessage());
-            }
-
-            if (!cls.isAssignableFrom(cls)) {
-                throw new RuntimeException();
-            }
-
-            Scrapper scrapper;
-            try {
-                scrapper = (Scrapper) cls.newInstance();
-                result.add(scrapper);
-            } catch (InstantiationException e) {
-                log.error(cls.getName() + " is not an implementation of Scrapper");
-            }
-        }
-
-        return result;
+    public Set<Scrapper> findClasses(String path) {
+        Set<Scrapper> scrapers = new HashSet<>();
+        findClassesInPath(new File(path), scrapers);
+        return scrapers;
     }
 
-    @SneakyThrows
-    public void v2(String path)  {
-        //print classpath
-        System.out.println(System.getProperty("java.class.path"));
-            Class<?> cls = Class.forName("src.main.java.main.Example");
+    private void findClassesInPath(File path, Set<Scrapper> scrapers) {
+        if (!path.exists()) {
+            return;
+        }
+
+        if (path.isDirectory()) {
+            File[] files = path.listFiles();
+            if (files != null) {
+                for (File file : files) {
+                    findClassesInPath(file, scrapers);
+                }
+            }
+        } else if (path.isFile() && path.getName().endsWith(".jar")) {
+            scrapers.addAll(findScrapersInJar(path));
+        }
+    }
+
+    private Set<Scrapper> findScrapersInJar(File jarFile) {
+        Set<Scrapper> scrapers = new HashSet<>();
+
+        try (JarFile jar = new JarFile(jarFile)) {
+            Enumeration<JarEntry> entries = jar.entries();
+
+            while (entries.hasMoreElements()) {
+                JarEntry entry = entries.nextElement();
+                if (!entry.isDirectory() && entry.getName().endsWith(".class")) {
+                    instantiateClassFromJar(jarFile, entry, scrapers);
+                }
+            }
+        } catch (IOException e) {
+            log.error("Error reading jar file: " + e.getMessage());
+        }
+
+        return scrapers;
+    }
+
+    private void instantiateClassFromJar(File jarFile, JarEntry entry, Set<Scrapper> scrapers) {
+        try {
+            Class<?> cls = loadClassFromJar(jarFile, entry.getName());
+            if (cls != null && Scrapper.class.isAssignableFrom(cls)) {
+                scrapers.add((Scrapper) cls.getDeclaredConstructor().newInstance());
+            }
+        } catch (InstantiationException | NoSuchMethodException | InvocationTargetException |
+                 IllegalAccessException e) {
+            log.error("Error instantiating class: " + e.getMessage());
+        }
+    }
+
+    private Class<?> loadClassFromJar(File jarFile, String className) {
+        try {
+            URLClassLoader classLoader = URLClassLoader.newInstance(new URL[]{jarFile.toURI().toURL()});
+            String canonicalClassName = className.replace("/", ".").replace(".class", "");
+            return Class.forName(canonicalClassName, true, classLoader);
+        } catch (IOException | ClassNotFoundException e) {
+            log.error("Error loading class from jar: " + e.getMessage());
+            return null;
+        }
     }
 }
