e331ac9c4f36e63de75e09eaa329f9ebc7a341d9
Maximiliano Sandoval <maxisandoval98@gmail.com>
2023-09-15 09:16:11 -0600

    Create Test.java

---
+++ src/test/java/Test.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CalculadoraTest {
+
+    @Test
+    public void exampleTest1() {
+        assertEquals(1, 1);
+    }
+
+    @Test
+    public void exampleTest2() {
+        assertEquals(2, 2);
+    }
+}
