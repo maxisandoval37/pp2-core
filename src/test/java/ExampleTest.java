bab4d84f235ca921fe277477708ae621998a2f9e
Maxi Sandoval <maxisandoval98@gmail.com>
2023-09-15 12:19:54 -0300

    updated example test

--- src/test/java/ExampleTest.java
+++ src/test/java/ExampleTest.java
@@ -1,7 +1,7 @@
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-public class CalculadoraTest {
+public class ExampleTest {
 
     @Test
     public void exampleTest1() {
