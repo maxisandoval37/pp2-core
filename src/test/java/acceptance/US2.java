07b259319c28f1d09d930411f85efcc22fb29e29
FrancoBre <bregolif.fb@gmail.com>
2023-09-15 12:55:54 -0300

    Add empty file for tests

--- src/test/java/acceptance/US2.java
+++ src/test/java/acceptance/US2.java
@@ -1,13 +1,16 @@
 package acceptance;
 
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import java.io.FileNotFoundException;
+import java.util.Set;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import service.discovery.ScraperDiscoverer;
 import shoppinator.core.interfaces.Scraper;
-import java.util.*;
-import static org.junit.jupiter.api.Assertions.*;
 
 
 class US2 {
@@ -25,23 +28,29 @@ class US2 {
 
         assertThrows(FileNotFoundException.class, () -> scraperDiscoverer.discover(nonExistingPath));
     }
+
     @Test
     void CA2() {
         String invalidPath = "a,,rc.h.i./.vo.txt";
         assertThrows(FileNotFoundException.class, () -> scraperDiscoverer.discover(invalidPath));
     }
+
     @Test
     void CA3() {
         String emptyFolderPath = "src/test/resources/empty-folder";
 
-        assertTrue(scraperDiscoverer.discover(emptyFolderPath).isEmpty());
+        var result = scraperDiscoverer.discover(emptyFolderPath);
+
+        assertTrue(result.isEmpty());
     }
+
     @Test
     void CA4() {
         String notScraperPath = "src/test/resources/not-scraper";
 
         assertTrue(scraperDiscoverer.discover(notScraperPath).isEmpty());
     }
+
     @Test
     void CA5() {
         String simpleScraperPath = "src/test/resources/simple-scraper";
@@ -49,6 +58,7 @@ class US2 {
         Set<Scraper> scrapers = scraperDiscoverer.discover(simpleScraperPath);
         assertEquals(1, scrapers.size());
     }
+
     @Test
     void CA6() {
         String multipleScraperPath = "src/test/resources/multiple-scraper";
