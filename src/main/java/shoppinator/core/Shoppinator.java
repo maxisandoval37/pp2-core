7420ba824240337903edb23d9a4d312a1a157ff3
FrancoBre <bregolif.fb@gmail.com>
2023-09-30 20:24:26 -0300

    Add Observer to Shoppinator for it to subscribe to shops

--- src/main/java/shoppinator/core/Shoppinator.java
+++ src/main/java/shoppinator/core/Shoppinator.java
@@ -1,18 +1,23 @@
 package shoppinator.core;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Observable;
+import java.util.Observer;
+import java.util.Set;
 import lombok.Getter;
 import service.discovery.ScraperDiscoverer;
 import shoppinator.core.factory.ShopFactory;
 import shoppinator.core.interfaces.Scraper;
 import shoppinator.core.interfaces.Shop;
-import java.util.Set;
 import shoppinator.core.model.Product;
 
-public class Shoppinator {
+@SuppressWarnings("deprecation")
+public class Shoppinator extends Observable implements Observer {
 
     @Getter
+    List<Product> products;
     Set<Shop> shops;
     ScraperDiscoverer scraperDiscoverer;
     ShopFactory shopFactory;
@@ -27,15 +32,28 @@ public class Shoppinator {
 
         Set<Scraper> scrapers = scraperDiscoverer.discover(path);
         this.shops = shopFactory.create(scrapers);
+        this.addObservers();
     }
 
     public List<Product> search(String productName) {
-        List<Product> products = new ArrayList<>();
+        this.products = new ArrayList<>();
         for (Shop shop : this.shops) {
             shop.search(productName);
-            products.addAll(shop.getProducts());
         }
 
         return products;
     }
+
+    @Override
+    public void update(Observable o, Object productList) {
+        this.products.addAll((List<Product>) productList);
+        products.sort(Comparator.comparing(p -> p.getProductPresentation().getPrice()));
+    }
+
+    private void addObservers() {
+        for (Shop shop : this.shops) {
+            shop.addObserver(this);
+        }
+    }
+
 }
