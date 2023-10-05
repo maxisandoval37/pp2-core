46b2a8471a190f49497f4dd0fbf1a1ee4c81fe76
FrancoBre <bregolif.fb@gmail.com>
2023-10-05 18:56:05 -0300

    add memento to searchCriteria

--- src/main/java/shoppinator/core/model/SearchCriteria.java
+++ src/main/java/shoppinator/core/model/SearchCriteria.java
@@ -8,6 +8,36 @@ import lombok.Data;
 public class SearchCriteria {
 
     private String productName;
-    private String priceMin;
-    private String priceMax;
+    private Long priceMin;
+    private Long priceMax;
+    private String[] shopNames;
+
+    public Memento saveState() {
+        return new Memento(this);
+    }
+
+    public void restoreState(Memento memento) {
+        this.productName = memento.getSearchCriteria().getProductName();
+        this.priceMin = memento.getSearchCriteria().getPriceMin();
+        this.priceMax = memento.getSearchCriteria().getPriceMax();
+        this.shopNames = memento.getSearchCriteria().getShopNames();
+    }
+
+    public static class Memento {
+        private final String productName;
+        private final Long priceMin;
+        private final Long priceMax;
+        private final String[] shopNames;
+
+        public Memento(SearchCriteria searchCriteria) {
+            this.productName = searchCriteria.getProductName();
+            this.priceMin = searchCriteria.getPriceMin();
+            this.priceMax = searchCriteria.getPriceMax();
+            this.shopNames = searchCriteria.getShopNames();
+        }
+
+        public SearchCriteria getSearchCriteria() {
+            return new SearchCriteria(productName, priceMin, priceMax, shopNames);
+        }
+    }
 }
