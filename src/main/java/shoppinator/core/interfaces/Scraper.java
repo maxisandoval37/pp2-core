5c42963899eec25359428798642940350a17e472
FrancoBre <bregolif.fb@gmail.com>
2023-10-09 22:07:40 -0300

    change scraper from abstract class to interface

--- src/main/java/shoppinator/core/interfaces/Scraper.java
+++ src/main/java/shoppinator/core/interfaces/Scraper.java
@@ -1,19 +1,6 @@
 package shoppinator.core.interfaces;
 
-public abstract class Scraper {
+public interface Scraper {
 
-    private String url;
-
-    public Scraper() {
-    }
-
-    public abstract String scrap(String productName);
-
-    public void setUrl(String url) {
-        this.url = url;
-    }
-
-    public String getUrl() {
-        return this.url;
-    }
+    String scrap(String productName);
 }
