70a6bc6472c0e1ba33686dadef39732b340b0121
Pablo Torres <ptorres@campus.ungs.edu.ar>
2023-10-15 09:12:12 -0300

    Update Shoppinator

--- src/main/java/shoppinator/core/Shoppinator.java
+++ src/main/java/shoppinator/core/Shoppinator.java
@@ -2,7 +2,9 @@ package shoppinator.core;
 
 import java.io.FileNotFoundException;
 import java.util.List;
+import java.util.Set;
 import shoppinator.core.facade.ShoppinatorFacade;
+import shoppinator.core.interfaces.Scraper;
 import shoppinator.core.model.Product;
 
 public class Shoppinator {
@@ -32,6 +34,7 @@ public class Shoppinator {
      *                  <li>The fourth parameter is the maximum price.</li>
      *                  <li>The remaining parameters are the selected shops.</li>
      *               </ol>
+     *               If no parameters are given, the last search parameters are used.
      */
     public List<Product> search(String... params) throws FileNotFoundException {
         return facade.search(params);
@@ -44,4 +47,15 @@ public class Shoppinator {
         return facade.getProductList();
     }
 
+    /**
+     * Subscribes an observer to the update of the core product list.
+     */
+    public void subscribe(Object observer) {
+        facade.subscribe(observer);
+    }
+
+    /*
+     * Returns the list of shops.
+     */
+    public Set<Scraper> getScrapers() { return facade.getScrapers(); }
 }
