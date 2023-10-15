7748de0b1acb03664db6f19fd0d5105291b31be1
Pablo Torres <ptorres@campus.ungs.edu.ar>
2023-10-15 09:18:02 -0300

    Update SearchCriteria

--- src/main/java/shoppinator/core/model/criteria/SearchCriteria.java
+++ src/main/java/shoppinator/core/model/criteria/SearchCriteria.java
@@ -1,13 +1,19 @@
 package shoppinator.core.model.criteria;
 
-import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 /**
  * This class is responsible for holding all the parameters
  * that the client passes, which is used in various parts of
  * the application.
  */
-@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class SearchCriteria {
 
     private String productName;
@@ -15,4 +21,29 @@ public class SearchCriteria {
     private FilterCriteria filterCriteria;
     private DiscoverCriteria discoverCriteria;
 
+    public Memento saveState() {
+        return new Memento(this);
+    }
+
+    public void restoreCriteria(Memento memento) {
+        this.productName = memento.getSearchCriteria().getProductName();
+        this.filterCriteria = memento.getSearchCriteria().getFilterCriteria();
+        this.discoverCriteria = memento.getSearchCriteria().getDiscoverCriteria();
+    }
+
+    public static class Memento {
+        private final String productName;
+        private final FilterCriteria filterCriteria;
+        private final DiscoverCriteria discoverCriteria;
+
+        public Memento(SearchCriteria searchCriteria) {
+            this.productName = searchCriteria.getProductName();
+            this.filterCriteria = searchCriteria.getFilterCriteria();
+            this.discoverCriteria = searchCriteria.getDiscoverCriteria();
+        }
+
+        public SearchCriteria getSearchCriteria() {
+            return new SearchCriteria(productName, filterCriteria, discoverCriteria);
+        }
+    }
 }
