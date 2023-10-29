0f9af6257e06971760fb46e0e216d09fdc76b0f5
FrancoBre <bregolif.fb@gmail.com>
2023-10-28 21:04:24 -0300

    borre todo lo de json

--- src/main/java/entities/Product.java
+++ src/main/java/entities/Product.java
@@ -1,6 +1,5 @@
 package entities;
 
-import com.fasterxml.jackson.annotation.JsonUnwrapped;
 import lombok.Data;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
@@ -9,9 +8,8 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Product {
+
     private String name;
     private String shopName;
-
-    @JsonUnwrapped
     private ProductPresentation productPresentation;
 }
