45cd3178cfd5ad5dd465c8efd2dfc8618b9725f4
FrancoBre <bregolif.fb@gmail.com>
2023-11-29 22:42:25 -0300

    Shop devuelve un único map/producto

--- src/main/java/entities/Shop.java
+++ src/main/java/entities/Shop.java
@@ -8,6 +8,5 @@ public interface Shop {
 
     Map<String, BigDecimal> search(String productName);
 
-    // INB4 https://stackoverflow.com/questions/1130294/java-interface-usage-guidelines-are-getters-and-setters-in-an-interface-bad
     String getName();
 }
